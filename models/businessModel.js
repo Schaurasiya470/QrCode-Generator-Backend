b63794a1862e753f336cab906d8df7c020f4fb5b
Sachin Chaurasiya <98591324+Schaurasiya470@users.noreply.github.com>
2025-09-14 23:31:40 +0530

    Update businessModel.js

--- models/businessModel.js
+++ models/businessModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true },
-    mobile: { type: Number, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    mobile: { type: Number, required: true},
+    email: { type: String, required: true},
     company: { type: String },
     websiteLink: { type: String},
 }, { timestamps: true });
